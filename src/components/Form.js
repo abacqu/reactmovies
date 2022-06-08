c23479a6e439d8f11eaf20af3867eb831864a66d
alec <abacqu0716@gmail.com>
2022-06-08 15:02:34 -0500

    two components added

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+// We Must Import the React Library
+import React from "react";
+
+// Define a function that is our component, always make sure to declare the props parameter so you can use props in your component
+const Form = (props) => {
+  //The component must return some JSX
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+};
+
+// We must export the component to use it in other files
+export default Form;
